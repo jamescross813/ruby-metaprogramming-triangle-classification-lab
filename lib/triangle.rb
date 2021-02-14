8ccf58d38f541a28eaa02e4ee80ad29ce78239e8
jamescross <crossjames813@gmail.com>
2021-02-14 14:07:46 -0600

    commit tree d77f414087132e0548a2ced1f9b8a281777111b5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
-end
+  # triangle code
+attr_reader :a, :b, :c
+  @lengths = []
+
+  def initialize(a, b, c)
+   
+    @a = a
+    @b = b
+    @c = c
+  end
+
+    def kind
+       triangle?
+      if @a == @b && @b == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+  def triangle?
+    triangle = [(a + b > c), (a + c > b), (c + b > a)]
+    [a, b, c].each do |side|
+      triangle << false if side <= 0 
+      raise TriangleError if triangle.include?(false)
+    end
+    
+  end
+    class TriangleError < StandardError
+      # triangle error code
+    end
+end 
